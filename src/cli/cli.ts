1b5acb4419cd931866004af56cae890e00cd8a9e
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-04-12 16:06:22 -0500

    added some log

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,12 +1,50 @@
 import { SystemAIService } from "../system/systemService.js";
+import { System } from "../config/config.js";
+import * as readline from "readline";
 
 class SystemCLI {
-    async systemRunner(): Promise<void> {
+    async systemRunner(msg: string): Promise<void> {
         const systemService = new SystemAIService();
-        const chat = await systemService.ollamaIntelligence("Hello chat!");
+        const chat = await systemService.ollamaIntelligence(msg);
         console.log(chat);
     }
 }
 
-const cli = new SystemCLI();
-void cli.systemRunner();
+class SystemRunner {
+    runner() {
+        const deviceName = System.SystemName;
+        const deviceDesc = System.SystemDesc;
+        console.log("This program has been initiated and is using: ", deviceName);
+        console.log(deviceDesc);
+
+        const messageAsInput = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: '> '
+        });
+
+        messageAsInput.prompt();
+
+        messageAsInput.on('line', async (input: string) => {
+            if (input.trim() === ':q') {
+                messageAsInput.close();
+                return;
+            }
+            console.log("--")
+            console.log("Thinking..... 😂")
+            console.log("--")
+
+            const cli = new SystemCLI();
+            await cli.systemRunner(input);
+            console.log("") // just to give some space
+            messageAsInput.prompt();
+        });
+
+        messageAsInput.on('close', () => {
+            console.log("Exiting...");
+        });
+    }
+}
+
+const system = new SystemRunner();
+system.runner();
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ const device = systemType();
 
 const System = {
     SystemName: device.name, // based on the device name we will open the requested shell
+    SystemDesc: device.desc,
     DEFAULT_PORT: 3000, // default port we will use for this system
 };
 
