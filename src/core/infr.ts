893ce0c9af38f7d807683a73f69efb319816de41
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-04-11 20:34:05 -0500

    infrustructure initiated

---
+++ src/core/infr.ts
@@ -0,0 +1,11 @@
+
+class Infrustructure {
+
+    async systemInfr() {
+        console.log("System infrusture - Test succeed!");
+    }
+
+}
+// Test for the infrustructure
+var infrusture = new Infrustructure();
+infrusture.systemInfr();
