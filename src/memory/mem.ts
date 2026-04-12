d8ba65caf0dfd31b657fbeb2cdb1d09e3ae43d6d
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-04-11 20:35:42 -0500

    added memory settings

---
+++ src/memory/mem.ts
@@ -0,0 +1,9 @@
+class MemoryManagemnt{
+    async memoryService(){
+        console.log("System memory management");
+    }
+}
+
+// system will be given memory
+var memoryManagemnt = new MemoryManagemnt();
+memoryManagemnt.memoryService();
