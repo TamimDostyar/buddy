d74f14068296c4656c3dbac84fb4a895e4a7b79e
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-06-01 21:43:40 -0500

    mcp

--- src/core/infr.ts
+++ src/core/infr.ts
@@ -1,11 +1,47 @@
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
-class Infrustructure {
 
-    async systemInfr() {
-        console.log("System infrusture - Test succeed!");
+
+interface MarkdownFiles{
+    filepath: string;
+    content: string;
+}
+
+
+class MCPStyle{
+    private skills;
+    private tasks;
+    
+    constructor(skill: string, task: string){
+        this.skills = skill;
+        this.tasks = task;
+    }
+
+
+    readSkillFile(){
+        const filepath = join(__dirname, this.skills);
+
+    }
+
+
+}
+    `
+        my goal in this ststen function is as followed:
+            -> call the main infr -> reads the entire skills no matter what -> decision part(class decide);
+            -> if task is given, then it needs to find the specific skill;
+            -> otherwise continues
+            -? leaving it incomplete for now
+    `
+
+
+class Infrustructure{
+    private skill;
+    private content;
+    constructor(skill: String, content: String){this.skill = skill, this.content = content};
+
+    systemInfr(){
+        var infrusture = new MCPStyle(this.skill, this.content);
     }
 
 }
-// Test for the infrustructure
-var infrusture = new Infrustructure();
-infrusture.systemInfr();
